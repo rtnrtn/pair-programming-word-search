02b7c159f063d33b9a1db9711ad60bda26b0c263
Tania Nardandrea-Isakovic <rtnrtn@gmail.com>
2020-07-29 22:01:05 +0000

    Fix initial buggy code and add ability to search for words vertically. Paired with Victoria.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,30 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+const transpose = function(matrix) {
+  if (matrix.length === 0) return [];
+  let newMatrix = [];
+  for (let column = 0; column < matrix[0].length; column++) {
+    let newSubMatrix = [];
+    for (let row = 0; row < matrix.length; row++) {
+      newSubMatrix.push(matrix[row][column]);
     }
-}
+    newMatrix.push(newSubMatrix);
+  }
+  return newMatrix;
+};
 
-module.exports = wordSearch
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) {
+      return true;
+    } else {
+      const verticalJoin = transpose(letters).map(ls => ls.join(''));
+      for (l of verticalJoin) {
+        if (l.includes(word)) {
+          return true;
+        }
+      }
+    }
+  } return false;
+};
+
+module.exports = wordSearch;
